069331d5cc5343287f45d5196aca4523da7a96f9
intiserrafi <intiserr@gmail.com>
2023-12-06 18:29:00 -0500

    homepage text change

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.use('/index2',express.static("views"))
-app.get('/', (req, res)=>{
-    res.sendfile('public/index.html');
-});
-app.get('/index', (req, res) =>{
-    res.sendfile('public/index.html');
-});
+app.use('/',express.static("views"))
+// app.get('/', (req, res)=>{
+//     res.sendfile('public/index.html');
+// });
+// app.get('/index', (req, res) =>{
+//     res.sendfile('public/index.html');
+// });
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
