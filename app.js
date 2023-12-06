fc0959b832230e6c5f6733062a7af95661116c0e
intiserrafi <intiserr@gmail.com>
2023-12-06 17:56:25 -0500

    index added

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = 8080;
 app.use('/index2',express.static("views"))
 app.get('/', (req, res) => res.send('Hello Wellcome WeThink Cloud by admin!!!'));
 app.get('/index', (req, res) =>{
-    res.render("index.ejs",{name:"Group 34"})
+    res.sendfile('public/index.html');
 });
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
