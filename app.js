35ac22bc0a5be44709d7528566f19dc069672102
intiserrafi <intiserr@gmail.com>
2023-12-06 18:15:22 -0500

    homepage updated

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express();
 const port = 8080;
 
 app.use('/index2',express.static("views"))
-app.get('/', (req, res) => res.send('Hello Wellcome WeThink Cloud by admin!!!'));
+app.get('/', (req, res)=>{
+    res.sendfile('public/index.html');
+});
 app.get('/index', (req, res) =>{
     res.sendfile('public/index.html');
 });
